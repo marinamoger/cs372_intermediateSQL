83dc32d40aa1c03ee3a3c753d2117a2037ea2b21
Marina Moger <your-email@example.com>
2025-10-27 17:28:44 -0700

    Submission 3

--- query3.sql
+++ query3.sql
@@ -1,6 +1,6 @@
 -- Write your query to select data here
-SELECT e.first_name, e.last_name, p.title AS project_title
-FROM employee AS e
-JOIN works_on AS w ON w.eid = e.id
-JOIN project  AS p ON p.id = w.pid
+SELECT e.first_name, e.last_name, p.title
+FROM employee e
+JOIN works_on w ON w.eid = e.id
+JOIN project  p ON p.id = w.pid
 ORDER BY e.first_name ASC, e.last_name ASC, p.title ASC;
